2421195756ae8cdc0cc39591886b1565f1448c20
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2021-10-20 15:35:33 -0300

    Update colors and themes
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

--- app/src/main/java/br/com/jms/contentprovider/presentation/MainActivity.kt
+++ app/src/main/java/br/com/jms/contentprovider/presentation/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         note_add.setOnClickListener {
             NotesDetailFragment().show(supportFragmentManager, "dialog")
