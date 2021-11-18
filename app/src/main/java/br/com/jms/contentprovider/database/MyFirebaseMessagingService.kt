c9b7313b196c04a9185f5a3a2895829dc5b3c5d7
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2021-11-18 12:25:11 -0300

    Add example of push notification using Firebase
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

---
+++ app/src/main/java/br/com/jms/contentprovider/database/MyFirebaseMessagingService.kt
@@ -0,0 +1,21 @@
+package br.com.jms.contentprovider.database
+
+import android.util.Log
+import br.com.jms.contentprovider.utils.showNotification
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val tag = "FirebaseMessagingServ"
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.i(tag, remoteMessage.from.toString())
+
+        if (remoteMessage.notification != null) {
+            this.showNotification("1234",
+                remoteMessage.notification?.title.toString(),
+                remoteMessage.notification?.body.toString()
+            )
+        }
+    }
+}
